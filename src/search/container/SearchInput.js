fc043ec32199377e9f7bfd2fc9af41671258675e
kmj <hi.minjungkim@gmail.com>
2020-09-05 00:48:28 +0900

    사용자 페이지 구현하기

--- src/search/container/SearchInput.js
+++ src/search/container/SearchInput.js
@@ -3,6 +3,8 @@ import { AutoComplete, Input, Space, Typography } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import { useSelector, useDispatch } from 'react-redux';
 import { actions } from '../state';
+import { actions as userActions } from '../../user/state';
+import { useHistory } from 'react-router-dom';
 
 export default function SearchInput() {
   const keyword = useSelector((state) => state.search.keyword);
@@ -15,15 +17,21 @@ export default function SearchInput() {
   }
 
   const autoCompletes = useSelector((state) => state.search.autoCompletes);
-  function gotoUser(value) {}
+  const history = useHistory();
+  function goToUser(value) {
+    const user = autoCompletes.find((item) => item.name === value);
+    if (user) {
+      dispatch(userActions.setValue('user', user));
+      history.push(`/user/${user.name}`);
+    }
+  }
 
   return (
     <AutoComplete
       value={keyword}
       onChange={setKeyword}
-      onSelect={gotoUser}
+      onSelect={goToUser}
       style={{ width: '100%' }}
-      // options={[]}
       options={autoCompletes.map((item) => ({
         value: item.name,
         label: (
